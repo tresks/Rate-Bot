d2a81eca670a82eb03d3200d1b292b354a47bc38
tresks <55102988+tresks@users.noreply.github.com>
2022-08-25 00:03:35 +0100

    Fixed
    Managed to find a new workaround for .Chatted so thought i'd just fix this

--- main.lua
+++ main.lua
@@ -1,29 +1,15 @@
 getgenv().rating = true
 getgenv().list = {}
 getgenv().blacklist = {}
-for i,v in pairs(game.Players:GetPlayers()) do
-    game.Players[v.Name].Chatted:connect(function(Message)
-        local sex = string.lower(Message)
-        if string.find(sex, "tresk rate me") and table.find(list, v.Name) ~= nil and getgenv().rating == true and table.find(blacklist, v.Name) == nil then
-            table.insert(blacklist, v.Name)
-            local args = {
-                [1] = "Becase you have already been rated by tresk's rating service your new rating is 1/10.",
-                [2] = "All"
-            }
-                
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-            wait(1)
-            local args = {
-                [1] = "Also because you have used the command again you have been blacklisted from using this please move along now.",
-                [2] = "All"
-            }
-                
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+game.ReplicatedStorage.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(function(messageData)
+    if messageData.FromSpeaker ~= game.Players.LocalPlayer.Name then
+        local message = string.lower(messageData.Message)
+
+        if string.find(message, "tresk rate me") and table.find(list, messageData.FromSpeaker) ~= nil and getgenv().rating == true and table.find(blacklist, messageData.FromSpeaker) == nil then
+            return
         else
-            if string.find(sex, "tresk rate me") and getgenv().rating == true and table.find(list, v.Name) == nil and table.find(blacklist, v.Name) == nil then
-                wait(0.1)
-                table.insert(list, v.Name)
-                print(list[1])
+            if string.find(message, "tresk rate me") and getgenv().rating == true and table.find(list, messageData.FromSpeaker) == nil and table.find(blacklist, messageData.FromSpeaker) == nil then
+                table.insert(list, messageData.FromSpeaker)
                 local number = math.random(1,10)
                 local args = {
                     [1] = "Welcome to tresk's rating service please wait while the bot checks your avatar.",
@@ -31,92 +17,32 @@ for i,v in pairs(game.Players:GetPlayers()) do
                 }
                     
                 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-                wait(1)
+                task.wait(1)
                 local args = {
                     [1] = "The bot has rated you "..number.."/10.",
                     [2] = "All"
                 }
                     
                 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-                local url =
-                    "WEBHOOK HERE"
+
                 local data = {
                     ["embeds"] = {
                         {
                             ["title"] = "**Player has been rated!**",
-                            ["description"] = "The bot has rated "..v.Name.." "..number.."/10",
+                            ["description"] = "The bot has rated "..messageData.FromSpeaker.." "..number.."/10",
                             ["type"] = "rich",
                             ["color"] = tonumber(0x7269da),
                         }
-                    }
+                    },
+                    ["avatar_url"] = "https://cdn.discordapp.com/attachments/984209136562602014/1012131586826846208/Rate_bot.png",
+                    ["username"] = "Rate Bot"
                 }
-                local newdata = game:GetService("HttpService"):JSONEncode(data)
-                
+
                 local headers = {
                     ["content-type"] = "application/json" 
                 }
-                request = http_request or request or HttpPost or syn.request
-                request({Url = url, Body = newdata, Method = "POST", Headers = headers})
+                syn.request({Url = "https://discord.com/api/webhooks/1011832551549186108/m9-2xMuDkJDvNuqWIq6YXDAcUGKoA907F5dV5NWwm8z_7kdBPjZIwI1Nn6yTcH-wJszV", Body = game:GetService("HttpService"):JSONEncode(data), Method = "POST", Headers = headers})
             end
         end
-    end)
-end
-game.Players.PlayerAdded:Connect(function(player)
-    game.Players[player.Name].Chatted:connect(function(Message)
-        local sex = string.lower(Message)
-        if string.find(sex, "tresk rate me") and table.find(list, player.Name) and getgenv().rating == true and table.find(blacklist, v.Name) == nil then
-            table.insert(blacklist, v.Name)
-            local args = {
-                [1] = "Becase you have already been rated by tresk's rating service you're new rating is.",
-                [2] = "All"
-            }
-                
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-            wait(1)
-            local args = {
-                [1] = "Also because you have used the command again you have been blacklisted from using this please move along now.",
-                [2] = "All"
-            }
-                
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-        else
-            if string.find(sex, "tresk rate me") and getgenv().rating == true and table.find(list, player.Name) == nil and table.find(blacklist, player.Name) == nil then
-                wait(0.1)
-                table.insert(list, player.Name)
-                local number = math.random(1,10)
-                local args = {
-                    [1] = "Welcome to tresk's rating service please wait while the bot checks your avatar.",
-                    [2] = "All"
-                }
-                    
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-                wait(1)
-                local args = {
-                    [1] = "The bot has rated you "..number.."/10.",
-                    [2] = "All"
-                }
-                    
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-                local url =
-                    "WEBHOOK HERE"
-                local data = {
-                    ["embeds"] = {
-                        {
-                            ["title"] = "**Player has been rated!**",
-                            ["description"] = "The bot has rated "..player.Name.." "..number.."/10",
-                            ["type"] = "rich",
-                            ["color"] = tonumber(0x7269da),
-                        }
-                    }
-                }
-                local newdata = game:GetService("HttpService"):JSONEncode(data)
-                
-                local headers = {
-                    ["content-type"] = "application/json" 
-                }
-                request = http_request or request or HttpPost or syn.request
-                request({Url = url, Body = newdata, Method = "POST", Headers = headers})
-            end
-        end
-    end)
+    end
 end)
